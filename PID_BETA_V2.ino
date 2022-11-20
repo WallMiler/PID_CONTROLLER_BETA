dbf7eb35269d83dc4af70954b304ef5e6f6e327b
WallMiler <59627998+WallMiler@users.noreply.github.com>
2022-11-20 14:19:45 -0300

    Update PID_BETA_V2.ino

--- PID_BETA_V2.ino
+++ PID_BETA_V2.ino
@@ -4,12 +4,17 @@
 
 //Definições das constantes
 #define fwd 5 //pino pwm
+#define rev 6 //pino pwm
 #define tac 0 //pino tacogerador
-#define pot 4 //pino potenciometro
+#define pot 1 //pino potenciometro
+#define bot 4 //botão para inversão da rotação
+#define rele 8
+#define rele_led 13 //led para sinalização dos relés
 #define n 20 //número de iterações
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 //Declaração para a função do filtro de média móvel
+int mode(); //declaração da função para reversão do sentido de giro do motor
 long moving_average(); //declaração do protótipo da função do filtro
 
 //variáveis do algoritmo das Equações das Diferenças
@@ -24,34 +29,50 @@ float cv, //  cv(n-1)
 float kp = 5;
 float ki = 2;
 float kd = 3.31;
-float ts = 0.1;
+float ts = 0.01;
 
 unsigned long timeold; //armazena tempo decorrido desde a inicialização
 
 int var_setpoint; //guarda valor do setpoint adquirido pelo potenciometro
+int relative_setpoint; //variável ponteiro para o setpoint
 int speed; //velocidade adquirida pelo tacogerador
+int pwm; //variável global para pwm
 
 //variáveis para filtro de média móvel
 int       original;      //armazena aquisição original   
 int       numbers[n];  //vetor volátil para o armazenamento dos dados das iterações
+
+bool last_botstate = 1; //estado anterior do botão
+bool bot_state; //estado atual do botão
+bool rele_state = 1; //estado atual do relé
+
+unsigned int last_time = 0; //ultimo tempo do millis() para o debounce
+unsigned int debounce_delay = 50; //tempo de delay do debounce
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 void setup(){
 timeold = 0; //inicia tempo de inicialização em 0
 
-pinMode(fwd, OUTPUT);
+pinMode(fwd, OUTPUT);//pino para ativar sentido horário
+pinMode(rev, OUTPUT);// pino para ativar sentido anti-horário
+pinMode(rele_led, OUTPUT); //saída para ativar relés da meia ponte h
+pinMode(bot, INPUT_PULLUP); //botão para ativar reversão
 Serial.begin(115200);
 }
 //----------------------------------------------------------------------------------------------------------------------------------------------------------
 void loop(){
 
   var_setpoint = analogRead(pot); //adquire setpoint variável pelo potenciometro
-  setpoint = map(var_setpoint, 0, 1023, 350, 900); //normalização do valor do setpoint, 325 foi a velocidade minima, 900 a máxima
+  relative_setpoint = mode(); //adquire novo valor de setpoint e inversão de rotação
+  setpoint = map(relative_setpoint, 0, 1023, 350, 900); //normalização do valor do setpoint, 325 foi a velocidade minima, 900 a máxima
+  
+
+  mode(); //chama a função para inveter o sentido de giro e alterar a velocidade
 
   original = analogRead(tac); //adqire dados do tacogerador
   
   speed = moving_average(); //armazena o valor retornado pela função do filtro
 
-  erro = setpoint - speed;
+  erro = setpoint - speed; //erro atual do sistema
 
   cv = cv_1 + (kp + kd/ts)*erro + (-kp + ki*ts - 2*kd/ts)*erro_1 + (kd/ts) * erro_2; //Equação das diferenças
   cv_1 = cv;
@@ -70,10 +91,10 @@ void loop(){
 
   if(speed < 340) cv = setpoint; //Se a velocidade for menor que o valor mínimo, cv será = setpoint
 
-  int pwm = map(cv, 0,setpoint, 106, 255);
+  pwm = map(cv, 0,setpoint, 106, 255);
   //analogWrite(fwd, cv*(255.0/800.0));//ativa o pwm para o pino designado
-  analogWrite(fwd, pwm);//ativa o pwm para o pino designado
-
+  //analogWrite(fwd, pwm);//ativa o pwm para o pino designado
+  
 //----------------------------------------------------------------------------------------------------------------------------------------------------------
 // Algoritmo de plotagem do gráfico serial e monitor serial
   if(millis() - timeold >= 10){
@@ -97,8 +118,6 @@ void loop(){
   Serial.println(cv);
 
   }
-  //delay que define a taxa de amostragem, valor também obtido de forma empírica
-  delay(10);
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -117,5 +136,48 @@ long moving_average()
 
    return acc/n; // realiza o retorna da operação de média, em que a soma acumulada dos vetores é dividida pelo número dos vetores
 
- 
-} 
+ } 
+
+int mode(){
+  int setpoint_mode; //variavel que guarda a velocidade do sistema
+  bool read_bot = digitalRead(bot); //variavel que faz a leitura e guarda o estado do botão
+  if(read_bot != last_botstate){ //faz a negação do estado do botão
+  last_time = millis(); //atualiza o tempo 
+  }
+ if ((millis() - last_time) > debounce_delay) { //compara os tempos para o debounce
+    if (read_bot != bot_state) { //
+      bot_state = read_bot;
+
+      if (bot_state == 1) {
+        rele_state = !rele_state;
+      }
+    }
+  }
+  digitalWrite(rele_led, rele_state); 
+  last_botstate = read_bot;
+
+  //Se o estado do relé for LOW o sentido de rotação do motor é horário
+  if(rele_state == LOW){ 
+      //setpoint_mode = 500;
+      digitalWrite(rele, LOW);
+      //digitalWrite(rev_led, LOW);
+      //digitalWrite(fwd_led, HIGH);
+      analogWrite(fwd, pwm);
+      analogWrite(rev, 0);
+      digitalWrite(rele_led, LOW);
+      setpoint_mode = var_setpoint - 150;
+  }
+
+  //Se o estado do relé for HIGH o sentido de rotação do motor é anti-horário
+  if(rele_state == HIGH){
+      //setpoint_mode = 500;
+      digitalWrite(rele, HIGH);
+      //digitalWrite(rev_led, HIGH);
+      //digitalWrite(fwd_led, LOW);
+      analogWrite(fwd, 0);
+      analogWrite(rev, pwm);
+      digitalWrite(rele_led, HIGH);
+      setpoint_mode = var_setpoint;
+  }
+  return setpoint_mode; //retorna novo setpoint
+}
